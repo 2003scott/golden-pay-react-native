84d836ae3fbd38e0f9f9ae768be138eb41ee0c70
Ckott <diegoangeles130703@gmail.com>
2025-01-13 19:46:26 -0500

    Agrega configuración de Axios y estructura básica de navegación en la aplicación

--- App.tsx
+++ App.tsx
@@ -6,14 +6,32 @@
  */
 
 import React from 'react';
-import { MainLayout } from './src/layouts';
-import { Navigation } from './src/routes';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
 function App(): JSX.Element {
     return (
-        <MainLayout>
-            <Navigation/>
-        </MainLayout>
+        <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
     )
 }
 
