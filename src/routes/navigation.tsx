a0451a686b463ed1b85056c6a73f63571fe57ea7
Ckott <diegoangeles130703@gmail.com>
2025-01-13 22:37:07 -0500

    Refactor navigation structure and update Android build configurations

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../modules/home';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Text, View } from 'react-native';
+import { enableScreens } from 'react-native-screens';
 
+enableScreens(false);
 const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
 
 const Navigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="home">
-                <Stack.Screen name="home"  component={Home}/>
+                <Stack.Screen name="home" component={HomeScreen} />
             </Stack.Navigator>
         </NavigationContainer>
-    )
-}
+    );
+};
 
-export { Navigation };
+export {Navigation};
